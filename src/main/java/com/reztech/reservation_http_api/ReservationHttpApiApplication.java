269ac826091d1d39c5623938bc24cd9a88ee6ac8
tuncerburak <tuncerburak97@gmail.com>
2025-06-20 00:10:46 +0300

    initial dep.

---
+++ src/main/java/com/reztech/reservation_http_api/ReservationHttpApiApplication.java
@@ -0,0 +1,13 @@
+package com.reztech.reservation_http_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationHttpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationHttpApiApplication.class, args);
+	}
+
+}
